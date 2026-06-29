0a27a660ea92719d2fdc7ec49bb9b7bb3aff75a8
Mike <kostyukovmihael@yandex.ru>
2026-06-29 21:37:24 +0300

    feat (GenerationController): add method for packing zip, also change port

--- src/main/java/ru/kostyukov/tankgenerator/controllers/GenerationController.java
+++ src/main/java/ru/kostyukov/tankgenerator/controllers/GenerationController.java
@@ -1,8 +1,11 @@
 package ru.kostyukov.tankgenerator.controllers;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -56,5 +59,28 @@ public class GenerationController {
         .body(zipFiles(ammo, loadYaml, readme));
   }
 
-  private byte[] zipFiles(String ammo, String loadYaml, String readme) {}
+  private byte[] zipFiles(String ammo, String loadYaml, String readme) throws IOException {
+    ByteArrayOutputStream byteOut = new ByteArrayOutputStream();
+
+    try (ZipOutputStream zipOut = new ZipOutputStream(byteOut)) {
+      ZipEntry ammoEntry = new ZipEntry("ammo.txt");
+      zipOut.putNextEntry(ammoEntry);
+      zipOut.write(ammo.getBytes(StandardCharsets.UTF_8));
+      zipOut.closeEntry();
+
+      ZipEntry loadYamlEntry = new ZipEntry("load.yaml");
+      zipOut.putNextEntry(loadYamlEntry);
+      zipOut.write(loadYaml.getBytes(StandardCharsets.UTF_8));
+      zipOut.closeEntry();
+
+      ZipEntry readmeEntry = new ZipEntry("README.md");
+      zipOut.putNextEntry(readmeEntry);
+      zipOut.write(readme.getBytes(StandardCharsets.UTF_8));
+      zipOut.closeEntry();
+
+      zipOut.finish();
+    }
+
+    return byteOut.toByteArray();
+  }
 }
