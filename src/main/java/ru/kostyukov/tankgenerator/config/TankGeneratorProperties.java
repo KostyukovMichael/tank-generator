f6e39778235cf7ebbbec2a4ddcdbd67b28ae37c8
Mike <kostyukovmihael@yandex.ru>
2026-07-03 17:59:38 +0300

    fix (TankGeneratorProperties): add @Setter, and remove 'final' from fields

--- src/main/java/ru/kostyukov/tankgenerator/config/TankGeneratorProperties.java
+++ src/main/java/ru/kostyukov/tankgenerator/config/TankGeneratorProperties.java
@@ -1,29 +1,31 @@
 package ru.kostyukov.tankgenerator.config;
 
 import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 @Getter
+@Setter
 @Component
-@ConfigurationProperties
+@ConfigurationProperties("tank")
 public class TankGeneratorProperties {
-  private final String defaultUserAgent = "Tank";
-  private final String defaultReplaceParameter = "1";
-  private final String defaultLoadType = "rps";
-  private final String defaultSchedule = "line";
-  private final int defaultRps = 50;
-  private final int defaultRpsStart = 1;
-  private final int defaultRpsEnd = 100;
-  private final int defaultRpsStep = 10;
-  private final String defaultDuration = "60s";
-  private final boolean defaultAutostopEnabled = false;
-  private final String defaultAutostopHttpCode = "5xx";
-  private final String defaultAutostopHttpLimit = "25%";
-  private final String defaultAutostopHttpWindow = "10s";
-  private final String defaultAutostopNetCode = "xx";
-  private final String defaultAutostopNetLimit = "25";
-  private final String defaultAutostopNetWindow = "10s";
-  private final String defaultAutostopResponseTime = "1500ms";
-  private final String defaultAutostopTimeWindow = "10s";
+  private String defaultUserAgent = "Tank";
+  private String defaultReplaceParameter = "1";
+  private String defaultLoadType = "rps";
+  private String defaultSchedule = "line";
+  private int defaultRps = 50;
+  private int defaultRpsStart = 1;
+  private int defaultRpsEnd = 100;
+  private int defaultRpsStep = 10;
+  private String defaultDuration = "60s";
+  private boolean defaultAutostopEnabled = false;
+  private String defaultAutostopHttpCode = "5xx";
+  private String defaultAutostopHttpLimit = "25%";
+  private String defaultAutostopHttpWindow = "10s";
+  private String defaultAutostopNetCode = "xx";
+  private String defaultAutostopNetLimit = "25";
+  private String defaultAutostopNetWindow = "10s";
+  private String defaultAutostopResponseTime = "1500ms";
+  private String defaultAutostopTimeWindow = "10s";
 }
